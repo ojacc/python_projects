fcd8be4d8aed0dab1d25b6490756dcdcd25a6c89
ojacc <58025708+ojacc@users.noreply.github.com>
2019-12-13 13:22:14 +0800

    Dice function defined like coin function.

--- coin_flip_project.py
+++ coin_flip_project.py
@@ -34,22 +34,41 @@ def coin_flip():
                 print("Current score:", wins_losses_count)
     return wins_losses_count
 
-coin_flip() 
-"""def roll_dice():
+#coin_flip() 
+
+
+def roll_dice():
     play_on = True
     wins_losses = ["Wins", "Losses"]
     count = [0, 0]
     while play_on:
         print("A 6-sided dice (1-6) is going to be rolled. Which outcome do you expect?")
-        user_guess = input()
-        if not user_guess in range(1,7):
-            print("Unvalid input. Please type an integer between 1 and 6.")
+        user_guess = int(input())
+        if not user_guess in [1, 2, 3, 4, 5, 6]:
+            print("Unvalid input. Please select an integer between 1 and 6.")
             continue
         dice_result = random.randint(1,6)
         print("Rolling...\nResult:", dice_result)
-        if dice_result == user_guess:
+        if user_guess == dice_result:
             count[0] += 1
-        elif """
+            print("Congratulations, you won!")
+        else:
+            count[1] += 1
+            print("You fucking loser!")
+        wins_losses_count = list(zip(wins_losses, count))
+        while True:
+            what_next = input("Choose one:\nA: Roll again\nB: Check score\nC: Exit game\n")
+            if what_next in ["A", "a", "Roll again", "roll again"]:
+                break
+            elif what_next in ["C", "c", "Exit game", "exit game"]:
+                print("Goodbye!")
+                play_on = False
+                break
+            elif what_next in ["B", "b", "Check score", "check score"]:
+                print("Current score:", wins_losses_count)
+    return wins_losses_count
+
+roll_dice()
 
 
 
